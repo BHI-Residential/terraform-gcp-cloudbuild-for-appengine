e1f8d543f7e8b1b36c9a9147196c83209db78cb0
angeleenasunny <129401128+angeleenasunny@users.noreply.github.com>
2023-09-15 14:25:52 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -30,7 +30,7 @@ locals {
     entrypoint = "bash"
     args = [
       "-c",
-      "curl -s https://github.com/BHI-Residential/gcp-cloudbuild-substitution-variables/v1.0.0/gcsvh.sh | bash"
+      "curl -s https://raw.githubusercontent.com/BHI-Residential/gcp-cloudbuild-substitution-variables/v1.0.0/gcsvh.sh | bash"
     ]
     env = [for k, v in local.all_vars : "${trim(k, "_")}=$${_${k}}"]
   }]
@@ -40,7 +40,7 @@ locals {
     entrypoint = "bash"
     args = [
       "-c",
-      "curl -s https://github.com/BHI-Residential/gcp-cloudbuild-substitution-variables/v1.0.0/gaefcv.sh | bash -s ${var.appengine_vpc_access} ${var.workspace}-vpc ${var.workspace}-${var.appengine_region}-private-subnet"
+      "curl -s https://raw.githubusercontent.com/BHI-Residential/gcp-cloudbuild-substitution-variables/v1.0.0/gaefcv.sh | bash -s ${var.appengine_vpc_access} ${var.workspace}-vpc ${var.workspace}-${var.appengine_region}-private-subnet"
     ]
   }
 
