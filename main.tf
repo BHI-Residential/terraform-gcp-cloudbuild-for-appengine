b6fd3b4b6c0262b7155d78028c8512e2380ef8a2
Angeleena <angeleena@qburst.com>
2023-09-18 10:35:03 +0530

    Repo based invocation using yaml files

--- main.tf
+++ main.tf
@@ -7,11 +7,12 @@ data "google_projects" "env_project" {
 }
 
 
+/*Trigger for repo level invocation*/
 resource "google_cloudbuild_trigger" "trigger" {
-  project         = local.project_name
-  service_account = "projects/${local.project_name}/serviceAccounts/${local.project_name}@appspot.gserviceaccount.com"
+ project         = local.project_name
+ service_account = "projects/${local.project_name}/serviceAccounts/${local.project_name}@appspot.gserviceaccount.com"
 
-  name = var.appengine_service_name
+ name = var.appengine_service_name
 
   github {
     name  = var.github_repository_name # Note: REPO must be connected first! Go to triggers -> Manage Repositories -> Ensure Global Region is Selected and then click on Connect Repository. DO NOT create a sample trigger!
@@ -22,51 +23,27 @@ resource "google_cloudbuild_trigger" "trigger" {
       invert_regex = false
     }
   }
-  
-
-
-  dynamic "build" {
-    for_each = toset([var.appengine_service_name])
-    content {
-      timeout = var.build_timeout
-      options {
-        logging = "STACKDRIVER_ONLY"
-        machine_type = var.machine_type
-      }
-
-      step {
-        name       = local.vpc_access_connector_step.name
-        entrypoint = local.vpc_access_connector_step.entrypoint
-        args       = local.vpc_access_connector_step.args
-      }
-
-      dynamic "step" {
-        for_each = length(local.all_vars) > 0 ? concat(local.variable_subsitition_step, var.build_steps) : var.build_steps
-        content {
-          # args - (optional) is a type of list of string
-          args = step.value["args"]
-          # entrypoint - (optional) is a type of string
-          entrypoint = step.value["entrypoint"]
-          # env - (optional) is a type of list of string
-          env = step.value["env"]
-          # name - (required) is a type of string
-          name = step.value["name"]
-        }
-      }
-
-
-
-    }
+  source_to_build {
+    uri       = var.uri
+    ref       = var.github_vcs_branch_regex
+    repo_type = "GITHUB"
   }
 
-
-
+  git_file_source {
+    path      = var.trigger_yaml_name
+    uri       = var.uri
+    revision  = var.github_vcs_branch_regex
+    repo_type = "GITHUB"
+  }
   substitutions = { for k, v in local.all_vars : "_${k}" => v }
 
+
+  // If this is set on a build, it will become pending when it is run, 
+  // and will need to be explicitly approved to start.
   approval_config {
     approval_required = contains(var.envs_requiring_build_approval, var.workspace) ? true : false
   }
-
+  
 }
 
 
--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "workspace" {}
-
+variable "uri"{}
 variable "gcp_folder_id" {}
 variable "github_org_name" {}
 variable "github_repository_name" {}
@@ -20,43 +20,13 @@ variable "envs_requiring_build_approval" {
 
 variable "plaintext" {}
 variable "encrypted" {}
-
+variable "trigger_yaml_name" {
+  
+}
 locals {
   plaintext = var.plaintext
   encrypted = var.encrypted
-
-  variable_subsitition_step = [{
-    name       = "launcher.gcr.io/google/ubuntu2004"
-    entrypoint = "bash"
-    args = [
-      "-c",
-      "curl -s https://raw.githubusercontent.com/GlueOps/gcp-cloudbuild-substitution-variables/v0.1.0/gcsvh.sh | bash"
-    ]
-    env = [for k, v in local.all_vars : "${trim(k, "_")}=$${_${k}}"]
-  }]
-
-  vpc_access_connector_step = {
-    name       = "launcher.gcr.io/google/ubuntu2004"
-    entrypoint = "bash"
-    args = [
-      "-c",
-      "curl -s https://raw.githubusercontent.com/GlueOps/gcp-app-engine-flexible-configure-vpc/v0.1.0/gaefcv.sh | bash -s ${var.appengine_vpc_access} ${var.workspace}-vpc ${var.workspace}-${var.appengine_region}-private-subnet"
-    ]
-  }
-
   plaintext_vars = [for key in local.plaintext[*] : { for k, v in key : k => v[var.workspace] }][0]
   secret_vars    = [for key in data.google_kms_secret.all_secrets[*] : { for k, v in key : k => v.plaintext }][0]
   all_vars       = merge(local.plaintext_vars, local.secret_vars)
-}
-
-variable "build_steps" {
-
-  type = list(object(
-    {
-      name       = string
-      entrypoint = string
-      args       = list(string)
-      env        = list(string)
-    }
-  ))
 }
