708d1c62869146c9ed77fc9f58afb7f227ca16a1
anand.gopinath <anand.gopinath@qburst.com>
2023-10-03 10:43:55 +0530

    ENG-407

--- alerts.tf
+++ alerts.tf
@@ -186,6 +186,9 @@ resource "google_monitoring_alert_policy" "app-engine-log-error-alerts" {
   conditions {
     display_name = "${local.project_name}-${var.appengine_service_name}-gae-log-errors"
     condition_threshold {
+      threshold_value         = var.alert_app_infra_threshold
+      comparison              = local.threshold_comparison.greater_than
+      duration                = "60s"
       filter = "resource.type = \"gae_app\" AND resource.labels.module_id = \"${var.appengine_service_name}\" AND severity= \"ERROR\" AND -httpRequest.requestUrl=\"/readiness_check\" AND -httpRequest.requestUrl=\"/liveness_check\" AND -(textPayload=~\"Health checks: instance=[^ =\\t\\n\\r\\f\\\"\\(\\)\\[\\]\\|']+ start=(\\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01]))T((\\d{2}):(\\d{2})(?::(\\d{2}(?:\\.\\d*)?))?(?:([+-](?:\\d{2}):?(?:\\d{2})?|Z)?) end=(\\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01]))T((\\d{2}):(\\d{2})(?::(\\d{2}(?:\\.\\d*)?))?(?:([+-](?:\\d{2}):?(?:\\d{2})?|Z)?) total=((?:\\d[,.]?)\\d) unhealthy=((?:\\d[,.]?)\\d) healthy=((?:\\d[,.]?)*\\d)\""
     }
   }
