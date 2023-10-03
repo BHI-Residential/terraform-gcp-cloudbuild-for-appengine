7c030882021e97adc9a914c14407f8bbe03002c9
angeleenasunny <129401128+angeleenasunny@users.noreply.github.com>
2023-10-03 19:15:16 +0530

    Update alerts.tf

--- alerts.tf
+++ alerts.tf
@@ -187,7 +187,7 @@ resource "google_monitoring_alert_policy" "app-engine-log-error-alerts" {
     condition_threshold {
       threshold_value = 1
       comparison      = "COMPARISON_GT" 
-      filter = "resource.type=\"gae_app\" AND resource.labels.module_id=\"${var.appengine_service_name}\" AND severity=\"ERROR\" AND NOT httpRequest.requestUrl=\"/readiness_check\" AND NOT httpRequest.requestUrl=\"/liveness_check\""
+      filter = "resource.type=\"gae_app\" AND resource.labels.module_id=\"${var.appengine_service_name}\" AND severity=\"ERROR\" AND NOT metadata.httpRequest.requestUrl=\"/readiness_check\" AND NOT metadata.httpRequest.requestUrl=\"/liveness_check\""
 
       aggregations {
         alignment_period     = "60s"  # Adjust the alignment period as needed
